10562bdbad83991988d9474b7ad126d899b1a6a8
Vytautas Astrauskas <vastrauskas@gmail.com>
2016-03-23 14:39:20 +0100

    Updated boogie script.

--- boogie.rb
+++ boogie.rb
@@ -4,14 +4,16 @@ class Boogie < Formula
   url "https://github.com/boogie-org/boogie.git"
   version "2.3"
 
+
   def install
 
     system "curl", "-L", "-o", "nuget.exe", "https://nuget.org/nuget.exe"
-    system "mono", "nuget.exe", "restore", "./Source/Boogie.sln"
-    system "xbuild", "Source/Boogie.sln"
+    system "/usr/local/bin/mono", "nuget.exe", "restore", "./Source/Boogie.sln"
+    system "/usr/local/bin/xbuild", "Source/Boogie.sln"
     system "mkdir", "#{prefix}/Binaries"
-    system "cp", "-r", "Binaries/", "#{prefix}/Binaries"
-    system "echo '#!/bin/sh'$'\\n''mono\ #{prefix}/Binaries/Boogie.exe\ \"$@\"'$'\\n' > #{prefix}/Binaries/boogie"
-    system "chmod", "+x", "#{prefix}/Binaries/boogie"
+    prefix.install Dir["Binaries/*"]
+    system "echo '#!/bin/sh'$'\\n''mono\ #{prefix}/Boogie.exe\ \"$@\"'$'\\n' > boogie"
+    system "chmod", "+x", "boogie"
+    bin.install "boogie"
   end
 end
